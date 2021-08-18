0b1b0cf9faa5846dbb8c2098362ea7eb08610124
James <jamesgeraghty@live.ie>
2021-08-18 19:16:55 +0100

    bug fixes

--- src/components/authHeader/index.js
+++ src/components/authHeader/index.js
@@ -8,7 +8,7 @@ const BaseAuthHeader = (props) => {
 
   return context.isAuthenticated ? (
     <p>
-      Welcome! <button onClick={() => context.signout()}>Sign out</button>
+      <button onClick={() => context.signout()}>Sign out</button>
     </p>
   ) : (
     <p>
---
+++ src/stories/authHeader.stories.js
@@ -0,0 +1,19 @@
+  
+import React from "react";
+import AuthHeader from "../components/authHeader";
+import SampleMovie from "./sampleData";
+import { MemoryRouter } from "react-router";
+//import MoviesContextProvider from "../contexts/moviesContext";
+
+export default {
+  title: "Authentication/AuthHeader",
+  component: AuthHeader,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+    //(Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
+  ],
+};
+
+export const Basic = () => <AuthHeader credit={SampleMovie} />;
+
+Basic.storyName = "Default";
